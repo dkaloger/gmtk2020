65e6a7be6b214c3599e34dae6859f5c9eaca9614
Nathan Moore <nathanmoore567@gmail.com>
2020-07-17 17:02:20 -0600

    Weed Vines 1.0
    First iteration of weed vines. They are a simple "physics joint rope" which isn't good enough for most games, but because our vines aren't moving much, nor is there going to be much physics-based interaction between these vines and the player, I'm sure this will prove sufficient.
    
    We're going to need some art to be able to get the vines to their full beauty. I'm thinking a end "cap" and some leaves single and group variety to be assembled in Unity itself.
    
    If the every art asset for the vines is rigged and exported with bones (no animations) that will allow for better IK and physics based animations. For example we could use some noise or Unity wind to generate dynamic animations for the leaves.

---
+++ Assets/Scripts/Vine.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Vine : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _segmentPrefab;
+
+    [SerializeField]
+    int _segments = 1;
+
+    [SerializeField]
+    float _segmentDistance = .21f;
+
+    [SerializeField]
+    bool _snapFirst, _snapLast;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Spawn ()
+	{
+        int count = (int)(_segments / _segmentDistance);
+
+        for (int i = 0; i < count; ++i)
+		{
+            GameObject temp = Instantiate(_segmentPrefab, new Vector3(transform.position.x, transform.position.y + _segmentDistance * (i + 1), transform.position.z), Quaternion.identity, transform);
+            temp.transform.eulerAngles = new Vector3(180, 0, 0);
+            temp.name = transform.childCount.ToString();
+
+            if (i == 0)
+			{
+                Destroy(temp.GetComponent<CharacterJoint>());
+
+                if(_snapFirst)
+                    temp.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			}
+			else
+			{
+                temp.GetComponent<CharacterJoint>().connectedBody = transform.Find((transform.childCount - 1).ToString()).GetComponent<Rigidbody>();
+			}
+        }
+
+        if(_snapLast)
+            transform.Find(transform.childCount.ToString()).GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+	}
+}
