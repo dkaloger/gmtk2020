ef16c50e41583c1373a1bc0fed1723f08cd19ead
Nathan Moore <nathanmoore567@gmail.com>
2020-07-16 18:22:32 -0600

    Player Farming Mechanic - WIP
    The player now interacts with triggers setup in the Player Interaction Trigger (Layer 8) to find the closes one to a point in front of her.
    This interaction become the target interaction if the player is to click (not yet implemented)
    You can see a sphere jumping around in the scene (not the game) to visually see to adjust the range and how it affects the user experiance.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,12 +14,21 @@ public class Player : MonoBehaviour
 	[SerializeField]
 	protected Transform _model; //this is a reference to the child of the player and the model from Mixamo
 
-	protected bool _hasWheelbarrow = false; //TODO: toggle this state when the player grabs the wheelbarrow.
+	protected bool _hasWheelbarrow = false; //TODO: toggle this state when the player grabs the wheelbarrow. //Possibly make a getter/setter so we can change the animator states to match this.
+
+	Vector3 _facing = Vector3.forward;
+
+	[SerializeField]
+	float _playerInteractionRange = 60f;
 
 	//reference to the animator used to control animations.
 	Animator _animator;
 
+	int _maxInteractionsToCheck = 10; // This determines how many colliders near the player can be checked to be made the "active" interaction item
+
 	Rigidbody _rb;
+
+	Transform _interactTarget;
 
 	private void Awake()
 	{
@@ -30,11 +40,33 @@ public class Player : MonoBehaviour
 	}
 
 	void FixedUpdate() {
-		
+
+		transform.rotation = Quaternion.LookRotation(_facing, Vector3.up);
+
+		Collider[] possibleInteractions = new Collider[_maxInteractionsToCheck];
+		int numInteractions = Physics.OverlapCapsuleNonAlloc(transform.position + Vector3.up * 50, transform.position + Vector3.down * 50, _playerInteractionRange, possibleInteractions, LayerMask.GetMask("Player Interaction Triggers"));
+		Debug.Log(numInteractions);
+
+		_interactTarget = null; 
+		Vector3 closestGoal = transform.position + transform.forward * (_playerInteractionRange / 2); //Using half the player interaction range as the target goal for what the player most wants to interact with.
+		float distance = Mathf.Infinity;
+		for (int i = 0; i < numInteractions; i++)
+		{
+			if (possibleInteractions[i].transform == transform)
+				continue; //We don't avoid ourself or the target.
+
+			//Get the closest interaction to the front of the player
+			if (Vector3.Distance(possibleInteractions[i].transform.position, closestGoal) < distance)
+			{
+				_interactTarget = possibleInteractions[i].transform;
+				distance = Vector3.Distance(possibleInteractions[i].transform.position, closestGoal);
+			}
+		}
 	}
 
 	public void OnMove(InputValue val) {
 		Vector2 move = val.Get<Vector2>();
+		
 		if (_hasWheelbarrow) //move with wheelbarrow
 		{
 			_animator.SetFloat("Horizontal", move.x);
@@ -42,7 +74,8 @@ public class Player : MonoBehaviour
 		}
 		else //Move normal
 		{
-			transform.rotation = Quaternion.LookRotation(new Vector3(move.x, 0, move.y), Vector3.up);
+			if (move.sqrMagnitude > 0f) 
+				_facing = new Vector3(move.x, 0, move.y); //only update facing if the player is inputing movement. This prevents the player from looking at 0,0,0 when no input is being given.
 			_animator.SetFloat("Speed", Mathf.Clamp(move.magnitude, 0f, 1f)); //tells the animator how fast we are going
 		}	
 	}
@@ -70,5 +103,20 @@ public class Player : MonoBehaviour
 		if (val.Get<float>() == 0)
 			return;
 				
+	}
+
+	/// <summary>
+	/// Draw Player Gizmos
+	/// </summary>
+	void OnDrawGizmosSelected()
+	{
+		//Draw a wire sphere representing the player's interaction range.
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, _playerInteractionRange);
+
+		if(_interactTarget != null)
+		{
+			Gizmos.DrawSphere(_interactTarget.position, 2f);
+		}
 	}
 }
