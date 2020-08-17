00448e856c8be32143e54b81e973f278a6b6f06e
IoriBranford <ioribranford@gmail.com>
2020-08-17 01:28:25 -0700

    Add money on collision with harvestable plant, in case that is preferred over using HarvestInteraction.

--- Assets/Scripts/ReadyState.cs
+++ Assets/Scripts/ReadyState.cs
@@ -7,13 +7,19 @@ using UnityEngine;
 /// </summary>
 public class HarvestState : ByTheTale.StateMachine.State
 {
-	public override void OnCollisionEnter(Collision collision)
+    public override void OnCollisionEnter(Collision collision)
 	{
 		base.OnCollisionEnter(collision);
 
-		//when we collide with the player we should be collected for all the money
+        //when we collide with the player we should be collected for all the money
 
-		
-	}
+        var finances = collision.collider.GetComponent<PlayerFinances>();
+        if (finances)
+        {
+            var plant = GetMachine<Plant>();
+            finances.DepositCash(plant.cashValue);
+            GameObject.Destroy(plant.gameObject);
+        }
+    }
 
 }
